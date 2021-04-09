e37ffafd423eec384feac342ca8c6222f2b43923
MateusBrasileiroNato <mateusbrasileironato@gmail.com>
2021-04-09 10:15:35 -0300

    Changes

--- if.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input('Digite o primeiro numero'))
-num2 = float(input('Digite o segundo numero'))
-if num1 == num2:
-    print('os numeros sao iguais')
-else:
-    print('os numeros sao diferentes')
